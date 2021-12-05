7665a1b93b2ac375fe65ca42c223ca932a1a6fc4
maykalgenchev <92879155+maykalgenchev@users.noreply.github.com>
2021-12-05 12:44:10 +0200

    services

--- src/services/songService.js
+++ src/services/songService.js
@@ -10,16 +10,55 @@ export const getAll = async () => {
     return result;
 };
 
-export const create = async (songData) => {
+export const create = async (songData, token) => {
     let response = await fetch(`${baseUrl}/songs`, {
         method: 'POST',
         headers: {
             'content-type': 'application/json',
+            'X-Authorization': token,
         },
-        body: JSON.stringify({ ...songData, likes: [] })
+        body: JSON.stringify({ ...songData })
     });
 
     let result = await response.json();
 
     return result;
 };
+
+export const edit = (songData, token) => {
+    return fetch(`${baseUrl}/songs/${songData._id}`, {
+        method: 'PUT',
+        headers: {
+            'content-type': 'application/json; charset=UTF-8',
+            'X-Authorization': token
+        },
+        body: JSON.stringify({ ...songData })
+    }).then(res => res.json());
+}
+
+export const getOne = (songId) => {
+    return fetch(`${baseUrl}/songs/${songId}`)
+        .then(res => res.json())
+};
+
+export const destroy = (songId, token) => {
+    return fetch(`${baseUrl}/songs/${songId}`, {
+        method: 'DELETE',
+        headers: {
+            'X-Authorization': token
+        }
+    }).then(res => res.json());
+}
+
+export const update = (songData, token) => {
+    return fetch(`${baseUrl}/songs/${songData._id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-Authorization': token
+        },
+        body: JSON.stringify({ ...songData }),
+    })
+        .then(res => res.json())
+};
+
