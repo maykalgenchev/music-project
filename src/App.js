54d3548e4e99023417df8eab6fad9eda75b9564c
maykalgenchev <92879155+maykalgenchev@users.noreply.github.com>
2021-12-05 12:46:23 +0200

    Header and App update

--- src/App.js
+++ src/App.js
@@ -1,30 +1,61 @@
 import { Routes, Route } from 'react-router-dom';
+import { AuthContext } from './contexts/AuthContext.js';
+
+import useLocalStorage from './hooks/useLocaleStorage.js';
 // import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header.js';
 import Login from './components/Login/Login.js';
 import Register from './components/Register/Register.js';
 import SongList from './components/SongList/SongList.js';
 import CreateSong from './components/Create/CreateSong.js';
+import Logout from './components/Logout/Logout.js';
+import Details from './components/Details/Details.js';
+import Edit from './components/Edit/Edit.js';
+import MySongs from './components/MySongs/MySongs.js';
+import LikedSongs from './components/LikedSongs/LikedSongs.js';
 
+
+const initialAuthState = {
+  _id: '',
+  email: '',
+  accessToken: '',
+};
 
 function App() {
+  let [user, setUser] = useLocalStorage('user', initialAuthState);
+
+  const login = (authData) => {
+    setUser(authData);
+  }
+
+  const logout = () => {
+    setUser(initialAuthState);
+  };
+
   return (
-    <div id="container">
-      <Header />
+    <AuthContext.Provider value={{ user, login, logout }}>
+      <div id="container">
+        <Header />
 
-      <main id="site-content">
-        <Routes>
-          <Route path="/all-songs" element={<SongList />} />
-          <Route path="/create" element={<CreateSong />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
-      </main>
+        <main id="site-content">
+          <Routes>
+            <Route path="/all-songs" element={<SongList />} />
+            <Route path="/my-songs" element={<MySongs />} />
+            <Route path="/create" element={<CreateSong />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/logout" element={<Logout />} />
+            <Route path="/details/:songId" element={<Details />} />
+            <Route path="/edit/:songId" element={<Edit />} />
+            <Route path="/liked-songs" element={<LikedSongs />} />
+          </Routes>
+        </main>
 
-      <footer id="site-footer">
-        <p>@SongWorld</p>
-      </footer>
-    </div>
+        <footer id="site-footer">
+          <p>@SongWorld</p>
+        </footer>
+      </div>
+    </AuthContext.Provider>
 
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,24 +1,51 @@
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../contexts/AuthContext';
+
 
 const Header = () => {
-    return (
-        <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 px-4 border-bottom">
-            <Link to="/" className="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none" />
-            <svg className="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"></svg>
+    const { user } = useContext(AuthContext);
+
+    let guestNavigation = (
+        <div id="guest" className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
 
 
-            <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-                <li><Link to="#" className="nav-link px-2 link-secondary" >Home</Link></li>
-                <li><Link to="/all-songs" className="nav-link px-2 link-dark" >All songs</Link></li>
-                <li><Link to="#" className="nav-link px-2 link-dark" >My songs</Link></li>
-                <li><Link to="/create" className="nav-link px-2 link-dark" >Add song</Link></li>
-                <li><Link to="#" className="nav-link px-2 link-dark" >Logout</Link></li>
-            </ul>
-
-            <div className="col-md-3 text-end">
+            <div className="text-end">
                 <button type="button" className="btn btn-outline-primary me-2"><Link to="/login">Login</Link></button>
                 <button type="button" className="btn btn-outline-primary me-2"><Link to="/register">Sign-up</Link></button>
             </div>
+
+
+        </div>
+    );
+
+    let userNavigation = (
+        <div id="user" className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+
+            <li><Link to="/my-songs" className="nav-link px-2 link-dark" >My songs</Link></li>
+            <li><Link to="/liked-songs" className="nav-link px-2 link-dark" >Liked songs</Link></li>
+            <li><Link to="/create" className="nav-link px-2 link-dark" >Add song</Link></li>
+            <li><Link to="/logout" className="nav-link px-2 link-dark" >Logout</Link></li>
+        </div>
+    );
+    return (
+        <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 px-4 border-bottom">
+            <nav>
+                <Link to="/" className="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none" />
+                <svg className="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"></svg>
+
+
+                <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+
+                    {user.email
+                        ? <li className="nav-link px-2 link-dark">Welcome, {user.email}</li>
+                        : ""}
+                    <li><Link to="/all-songs" className="nav-link px-2 link-dark" >All songs</Link></li>
+                    {user.email
+                        ? userNavigation
+                        : guestNavigation}
+                </ul>
+            </nav>
         </header>
     )
 
