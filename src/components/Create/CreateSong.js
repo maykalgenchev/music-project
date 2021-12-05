5f76ca5a9cfe93f7c7359feea5783d6a278ce3a8
maykalgenchev <92879155+maykalgenchev@users.noreply.github.com>
2021-12-05 12:47:51 +0200

    Creat song functionality

--- src/components/Create/CreateSong.js
+++ src/components/Create/CreateSong.js
@@ -1,8 +1,11 @@
 import { useNavigate } from 'react-router-dom';
 import * as songService from '../../services/songService';
-
+import { AuthContext } from '../../contexts/AuthContext';
+import { useContext } from 'react';
 const CreateSong = () => {
     const navigate = useNavigate();
+    const { user } = useContext(AuthContext)
+
     const onSongCreate = (e) => {
         e.preventDefault();
 
@@ -20,35 +23,37 @@ const CreateSong = () => {
             description,
             type,
             imageUrl,
-        }).then(result => {
+            likes: [],
+            _ownerId: user._id
+        }, user.accessToken).then(result => {
             navigate('/all-songs');
         })
     }
 
     return (
         <form onSubmit={onSongCreate} method="POST">
-            <div class="form-group">
+            <div className="form-group">
                 <label>Song name</label>
-                <input type="text" class="form-control" id="name" name="name" placeholder="Enter song name..." />
+                <input type="text" className="form-control" id="name" name="name" placeholder="Enter song name..." />
             </div>
-            <div class="form-group">
+            <div className="form-group">
                 <label>Song author</label>
-                <input type="text" class="form-control" id="author" name="author" placeholder="Enter song author..." />
+                <input type="text" className="form-control" id="author" name="author" placeholder="Enter song author..." />
             </div>
-            <div class="form-group">
+            <div className="form-group">
                 <label>Description</label>
-                <input type="text" class="form-control" id="description" name="description" placeholder="Enter description..." />
+                <input type="text" className="form-control" id="description" name="description" placeholder="Enter description..." />
             </div>
-            <div class="form-group">
+            <div className="form-group">
                 <label>Type</label>
-                <input type="text" class="form-control" id="type" name="type" placeholder="Enter type..." />
+                <input type="text" className="form-control" id="type" name="type" placeholder="Enter type..." />
             </div>
-            <div class="form-group">
+            <div className="form-group">
                 <label>Image Url</label>
-                <input type="text" class="form-control" id="imageUrl" name="imageUrl" placeholder="Enter imageUrl..." />
+                <input type="text" className="form-control" id="imageUrl" name="imageUrl" placeholder="Enter imageUrl..." />
             </div>
 
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <button type="submit" className="btn btn-primary">Submit</button>
         </form>
     )
 
