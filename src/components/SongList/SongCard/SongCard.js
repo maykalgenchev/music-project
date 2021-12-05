ca8d5a654e61b0bab0d46a88247aa9546d7602d9
maykalgenchev <92879155+maykalgenchev@users.noreply.github.com>
2021-12-05 12:47:05 +0200

    Song card update

--- src/components/SongList/SongCard/SongCard.js
+++ src/components/SongList/SongCard/SongCard.js
@@ -3,15 +3,16 @@ import { Link } from 'react-router-dom';
 const SongCard = ({
     song
 }) => {
-    return (
+    console.log('Song: ', song)
 
+    return (
         <div className="card p5" style={{ width: "13rem" }}>
             <img className="card-img-top" src={song.imageUrl} alt="Card cap" />
             <div className="card-body">
                 <h5 className="card-title">{song.name}</h5>
                 <p className="card-text">Author: {song.author}</p>
                 <p className="card-text">Type: {song.type}</p>
-                <Link to="/login" className="btn btn-primary">Details</Link>
+                <Link to={`/details/${song._id}`} className="btn btn-primary">Details</Link>
             </div>
         </div>
 
